07a1aa460469228ca0907dc01bef87940c3524d6
{css3studio} <css3studio@gmail.com>
2021-11-25 15:17:43 +0800

    메인

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,40 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      fontSize: {
-        sm: ['14px', '20px'],
-        base: ['16px', '24px'],
-        lg: ['16px', '24px'],
-        xl: ['16px', '24px'],
+    container: {
+      center: true,
+      screens: {
+        sm: "100%",
+        md: "100%",
+        lg: "992px",
+        xl: "1240px"
       },
+      padding: {
+        DEFAULT: "16px",
+        sm: "16px",
+        md: "16px",
+        lg: "0",
+        xl: "0"
+      }
+    },
+    extend: {
       colors: {
         'aa-gray-50': '#FAFAFA',
         'aa-gray-100': '#F5F5F5',
         'aa-gray-200': '#E5E5E5',
         'aa-gray-300': '#D4D4D4',
+        'aa-gray-400': '#A3A3A3',
+        'aa-gray-500': '#737373',
+        'aa-gray-600': '#525252',
+        'aa-gray-700': '#404040',
+        'aa-gray-800': '#262626',
+        'aa-gray-900': '#171717',
         'aa-purple-50': '#E7D8FF',
         'aa-purple-500': '#8A64EA',
+        'aa-purple-600': '#7045DE',
+        'aa-green-500': '#10B981',
+        'aa-red-500': '#EF4444',
+        'aa-yellow-400': '#FBBF24',
       },
       fontFamily: {
         sans: ["Spoqa Han Sans Neo", "sans-serif"],
@@ -26,5 +46,7 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/line-clamp'),
+  ],
 }
